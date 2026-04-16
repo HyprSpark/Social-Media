b24ccef010275c5eb07f6c3876f9978d4213bcd0
Alex Ferguey <alex.j.ferguey@gmail.com>
2026-04-16 19:12:16 +0100

    Fixed sort by newest strategy

--- include/strategies/NewestStrategy.h
+++ include/strategies/NewestStrategy.h
@@ -2,11 +2,16 @@
 #include <QList>
 #include "models/Post.h"
 #include "FeedStrategy.h"
+#include <algorithm>
 
 class NewestStrategy : public FeedStrategy {
 public:
     void sort(QList<Post>& posts, const User& currentUser) override {
-        
+        std::sort(posts.begin(), posts.end(), [](const Post& a, const Post& b) {
+            QDateTime dtA = QDateTime::fromString(a.timestamp, "MMM dd, HH:mm");
+            QDateTime dtB = QDateTime::fromString(b.timestamp, "MMM dd, HH:mm");
+            return dtA > dtB; 
+            });
     }
 };
 
